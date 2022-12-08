465b51258010f93bbb60956876be84ad85010673
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-12-08 12:35:40 +0100

    2+

--- js/task-02.js
+++ js/task-02.js
@@ -7,8 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
-
-
-
-
 const listGoods = document.querySelector('#ingredients');
+
+const elem = ingredients.map(option => {
+  const liEl = document.createElement('li');
+  liEl.classList.add('item');
+  liEl.textContent = option;
+  return liEl;
+});
+console.log(elem);
+listGoods.append(...elem);
